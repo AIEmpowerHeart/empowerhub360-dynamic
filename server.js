f32fb665a0bb22738a0b83bcbdd826cd53034b6d
AIEmpowerHeart <aiempowerheart360@gmail.com>
2025-05-06 05:03:25 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
-// Routes
 app.get('/', (req, res) => {
-    res.render('index', { title: "EmpowerHub360" });
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log('Server running...');
 });
